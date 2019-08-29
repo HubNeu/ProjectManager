36b870aef460a50dbb9ba1fa1a7c7fee7ce5e776
HubNeu <hub.neubauer@gmail.com>
2019-08-29 17:55:48 +0200

    Fixed pathing and fxmlloader.load finally works

--- src/main/java/com/hubert/neubauer/Main.java
+++ src/main/java/com/hubert/neubauer/Main.java
@@ -2,28 +2,26 @@ package com.hubert.neubauer;
 
 /**
 Problems:
--packaged jar doesn't see sample.fxml, something to do with path
+-none,perfect,flawless
 
  To do:
- -add this using git to my github
- -fix and implement fxml controller that will change scenes
+ -implement fxml controller that will change scenes and get on with finally building it //29.08.2019
  */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setScene(new Scene(new Pane(), 300, 275));
         primaryStage.show();
+        System.out.println(System.getProperty("user.dir"));
     }
 
 
